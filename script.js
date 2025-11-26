eb6718d99df429cbb8b7b871fc44affd10bae3c1
poulomi-codingal <poulomi.codingal@gmail.com>
2025-11-26 14:53:22 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+//array declation and printing ...
+const cars = ["BMW", "Maruti", "Kia", "Honda"];
+console.log(cars);
+
+//Methods in Array
+console.log(cars.length)
+console.log(cars.toString())
+//console.log(cars.push("Tata"))
+console.log(cars.join('*'))
+console.log(cars.pop())
+console.log(cars.push("Tata"))
+console.log(cars.shift())
+console.log(cars.unshift("Tesla"))
+
+const country = ['India', 'USA', 'China'];
+console.log(cars.concat(country))
+
+console.log(cars.sort())
+console.log(cars.reverse())
